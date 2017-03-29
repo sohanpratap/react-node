8bfc5eef08a03838b1ba55e087b42e0c1012c9bb
msingh <mohanpratap.singh@excelindia.com>
2017-03-29 16:49:42 +0530

    Final ui

--- app/components/ItemInfo.jsx
+++ app/components/ItemInfo.jsx
@@ -7,17 +7,18 @@ module.exports = React.createClass({
                 <div className="item-simple">
                     <input type="checkbox" name="menuItem[]" value="{this.props.info.id}"/>
                     <div className="item-simple-content">
-                        <div className="item-detail col-sm-6">
+                        <div className="item-detail col-sm-4">
                             <a href="#" className="pull-left">
                                 <img alt=""
                                      src="http://preview.byaviators.com/template/realsite/assets/img/tmp/medium/1.jpg"/>
                             </a>
                         </div>
-                        <div className="item-detail col-sm-6">
+                        <div className="item-detail col-sm-8">
                             <h2 className="item-simple-title">{this.props.info.name}</h2>
                             <ul className="item-simple-location">
                                 <li><a href="#">{this.props.info.tagline}</a>,</li>
                             </ul>
+                            <p className="hline"></p>
                             <p>
                                 <span className="currency-color pull-left">&#x20b9;&#x20b9;&#x20b9; &nbsp;&nbsp; </span>
                                 <span className="star-option">
